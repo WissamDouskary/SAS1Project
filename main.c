239a3ac71a5b8a75575a5f041ebb19093755c915
WissamDouskary <wissamdouskary99@gmail.com>
2024-09-11 09:09:09 +0100

    'firstadd'

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_ELEVE 100
+#define MAX 100
+typedef struct {
+    int numunique;
+    char nom[MAX];
+    char prenom[MAX];
+    char notegenerale[MAX];
+    float generalnote[MAX]
+}student;
+
+int e=0;
+student students[MAX_ELEVE];
+// 1. Ajoute un eleve :
+void Ajouter_eleve(){
+
+}
+
+int main()
+{
+    return 0;
+}
